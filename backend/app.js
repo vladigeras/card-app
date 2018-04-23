9407edb1f14e06e550e5cd1a060eec7f1f7612cf
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-04-23 21:22:26 +0300

    Getting data from server

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
-let express = require('express');
 
+let express = require('express');
 let app = express();
 let port = process.env.PORT || 5050;
 
-app.get('/api', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+let Card = require('./model/Card');
+
+app.get('/api/cards', (request, response) => {
+    let cards = [];
+
+    cards.push(new Card(1, "sdg", "dsg", "dsh", "wh"));
+    cards.push(new Card(2, "sdg", "dsg", "dsh", "wh"));
+    cards.push(new Card(3, "sdg", "dsg", "dsh", "wh"));
+    cards.push(new Card(4, "sdg", "dsg", "dsh", "wh"));
+    cards.push(new Card(5, "sdg", "dsg", "dsh", "wh"));
+    cards.push(new Card(6, "sdg", "dsg", "dsh", "wh"));
+
+    response.setHeader('Content-Type', 'application/json');
+    response.send({items: cards});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
